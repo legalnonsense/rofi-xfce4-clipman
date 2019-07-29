0d36d70fc6609f52b168b24ce648e16e74144510
Jeff Filipovits <jrfilipovits@gmail.com>
2019-07-28 23:52:41 -0400

    ready to test

--- rofi-clipman-history.sh
+++ rofi-clipman-history.sh
@@ -3,5 +3,8 @@
 
 open=$(./rofi-clipman-history-splitter.py | rofi -dmenu "$@" -no-sort -l 50 -i -p "Copy:" format -q -width 2500)
 
-echo open | xclip -sel clip
+echo $open | xclip -sel clip
+
+
+
 
